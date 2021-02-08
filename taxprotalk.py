e260790d50bbd845a9d448b70c90cd8a9ac83fd0
rookie <rookie@Xiaoke.local>
2021-02-08 10:42:41 +0800

    add app

---
+++ taxprotalk.py
@@ -0,0 +1,103 @@
+from Request import Request
+from bs4 import BeautifulSoup
+import time
+
+request = Request()
+
+host= 'https://www.taxprotalk.com/forums'
+list_links = []
+post_api = 'https://www.ty18198.com/wordpress_post_json_api.php?action=save'
+cate = ''
+
+def getList(url):
+    global list_links
+    response = request.request(url=url)
+    if response == False:
+        return False
+
+    soup = BeautifulSoup(response,'html.parser')
+    links = soup.find_all('a',attrs={'class','topictitle'})
+    list_links = [host + item.get('href').lstrip('.') for item in links][2:]
+
+def bodyAndRepay(title,usernames,post_dates,post_content):
+    body = {
+        'post_title':title,
+        'post_author':usernames[0],
+        'post_date':post_dates[0],
+        'post_content': post_content[0]
+    }
+    repay = []
+    for i in range(1,len(usernames)):
+        repay.append({
+            'author':usernames[i],
+            'date':post_dates[i],
+            'content': post_content[i]
+        })
+    usernames = []
+    post_content = []
+    post_dates = []
+    body['replys'] = repay
+    return body
+
+def bodyFormat(soup):
+    title = soup.find('h2').next.text
+    names = soup.find_all('div',attrs={'class','tpt-whome'})
+    usernames = [item.contents[1].text for item in names]
+    names = []
+    dates = soup.find_all('span',attrs={'class','tpt-postdate'})
+    post_dates = [time.strftime('%Y-%m-%d %H:%M',time.strptime(item.text.rstrip('\n\t\t\t\t\t').replace('\n\xa0',''),"%d-%b-%Y %I:%M%p")) for item in dates]
+    body = soup.find_all('div',attrs={'class','content'})
+    post_content = [str(item) for item in body]
+    body = []
+    return bodyAndRepay(title,usernames,post_dates,post_content)
+
+
+def body():
+    global list_links, cate
+    for url in list_links:
+        response = request.request(url=url)
+        if response == False:
+            continue
+
+        soup = BeautifulSoup(response,'html.parser')
+        try:
+            data = bodyFormat(soup)
+            data['post_category'] = cate
+            response = request.requestPostJson(api=post_api,json=data)
+            request.logger.info('Post Success,title:{}'.format(data['post_title']))
+        except Exception as e:
+            request.logger.error('body format error,msg:{}'.format(e))
+        soup = ''
+        response = ''
+    list_links = []
+
+def content(url):
+    getList(url)
+    if len(list_links) <= 0 :
+        request.logger.error('list links is empty')
+        return False
+    body()
+
+def main():
+    global cate
+    list = [{
+            'cate': 'Taxation',
+            'url': 'https://www.taxprotalk.com/forums/viewforum.php?f=8'
+            },
+            {
+            'cate': 'General Accounting',
+            'url': 'https://www.taxprotalk.com/forums/viewforum.php?f=9'
+            },
+            {
+            'cate': 'Business Operations and Development',
+            'url': 'https://www.taxprotalk.com/forums/viewforum.php?f=10'
+            },
+        ]
+    for item in list:
+        cate = item['cate']      
+        content(item['url'])
+        request.logger.info('reptile success,cate:{}'.format(item['cate']))
+        time.sleep(2)
+        
+        
+main()
---
+++ thetaxbook.py
@@ -0,0 +1,83 @@
+from Request import Request
+from bs4 import BeautifulSoup
+import time
+
+request = Request()
+list_links = []
+post_api = 'https://www.ty18198.com/wordpress_post_json_api.php?action=save'
+
+def getList(url):
+    global list_links
+    headers = {
+        'Host':'forum.thetaxbook.com',
+        'Referer':'https://forum.thetaxbook.com/forum/discussion-forums/main-forum-tax-discussion/304111-preparer-responsible-for-client-penalty'
+    }
+
+    response = request.request(url=url,headers=headers)
+    if response == False:
+        return False
+
+    soup = BeautifulSoup(response,'html.parser')
+    links = soup.find_all('a',attrs={'class','topic-title js-topic-title'})
+    list_links = [item.get('href') for item in links]
+
+def bodyFormat(soup):
+    post_title = soup.find('h2',attrs={'class','b-post__title js-post-title b-post__hide-when-deleted'}).text.replace('\r','').replace('\n','').replace('\t','')
+    author = soup.find_all('div',attrs={'class','author h-text-size--14'})
+    post_author = [item.contents[1].text for item in author]
+    author = []
+    dates = soup.find_all('time',attrs={'itemprop':'dateCreated'})
+    post_date = [item.get('datetime').replace('T',' ') for item in dates]
+    dates = []
+    body = soup.find_all('div',attrs={'class','js-post__content-text restore h-wordwrap'})
+    post_content = [str(item) for item in body]
+    body = []
+    return {'post_title':post_title,'post_author':post_author,'post_date':post_date,'post_content':post_content}
+
+def resultFormat(data):
+    body = {
+        'post_title':data['post_title'],
+        'post_author':data['post_author'][0],
+        'post_date':data['post_date'][0],
+        'post_content': data['post_content'][0],
+        'replys':[]
+    }
+    for i in range(1,len(data['post_author'])):
+        body['replys'].append({
+            'author':data['post_author'][i],
+            'date':data['post_date'][i],
+            'content': data['post_content'][i]
+        })
+    data = []
+    return body
+
+def body():
+    global list_links
+    for url in list_links:
+        response = request.request(url=url)
+        if response == False:
+            continue
+
+        try:
+            soup = BeautifulSoup(response,'html.parser')
+            data = bodyFormat(soup)
+            data = resultFormat(data)
+            data['post_category'] = 'Taxation'
+            request.requestPostJson(api=post_api,json=data)
+            request.logger.info('Post Success,TITLE:{}'.format(data['post_title']))
+        except Exception as e:
+            request.logger.error('body format error,msg:{}'.format(e))
+        soup = ''
+        data = ''
+        response = ''
+    list_links = []
+
+def main():  
+    url = 'https://forum.thetaxbook.com/forum/discussion-forums/main-forum-tax-discussion/page1'
+    getList(url)
+    if len(list_links) <= 0:
+        exit()
+    body()
+    request.logger.info('reptile success')
+
+main()
